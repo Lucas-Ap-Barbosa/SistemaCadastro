27ae54a510d4c6915bfcf1c9ef3227d45532690e
Lucas-Ap-Barbosa <lucasapbarbosa24@gmail.com>
2026-05-23 01:06:18 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddAuthorization();
 
 // 🔹 Conexão com banco
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 var app = builder.Build();
 
